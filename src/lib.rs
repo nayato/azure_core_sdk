31fb3157942956750a6efb159229710262cde4ac
Francesco Cogno <francesco.cogno@outlook.com>
2018-02-20 12:27:39 +0100

    exported COMPLETE_ENCODE_SET

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,15 @@ extern crate futures;
 extern crate hyper;
 extern crate native_tls;
 extern crate serde_json;
+#[macro_use]
 extern crate url;
 extern crate uuid;
 extern crate xml;
 
 #[macro_use]
-extern crate quick_error;
-#[macro_use]
 extern crate log;
+#[macro_use]
+extern crate quick_error;
 
 pub mod ba512_range;
 #[macro_use]
@@ -27,3 +28,10 @@ pub mod incompletevector;
 pub mod lease;
 pub mod parsing;
 pub mod range;
+
+use url::percent_encoding;
+define_encode_set! {
+        pub COMPLETE_ENCODE_SET = [percent_encoding::USERINFO_ENCODE_SET] | {
+                    '+', '-', '&'
+                            }
+}
